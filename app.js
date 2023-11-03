7a013e849f5644a8a3048a0b84d5f9bb02f8ec6a
jnavarro30 <jnavarro30@live.com>
2023-11-03 16:58:11 -0700

    checkpoint

---
+++ app.js
@@ -0,0 +1,5 @@
+const test = document.querySelector("div[text='this text']");
+
+setTimeout(() => {
+    console.log(test, 'hey')
+}, 2000)
---
+++ how-to.js
@@ -0,0 +1,18 @@
+// import puppeteer from "puppeteer";
+
+// const run = async() => {
+//     const browser = await puppeteer.launch();
+//     const page = await browser.newPage();
+//     await page.goto("https://portal.independa.com/");
+
+//     await page.screenshot({ path: 'example.png', fullPage: true });
+//     await page.screenshot({ path: 'example.pdf', format: 'A4' });
+
+// await page.setViewport({width: 1080, height: 1024});
+// await page.type('.search-box__input', 'automate beyond recorder');
+
+
+//     await browser.close();
+// }
+
+// run();
